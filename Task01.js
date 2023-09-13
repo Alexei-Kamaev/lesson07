cdd4033ba59ba844d9688a138bf077f99672a736
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-13 09:55:18 +0300

    первая задача

--- Task01.js
+++ Task01.js
@@ -1,21 +1,17 @@
 'use strict';
-
-
 {
 const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
 const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
 function filter (firstArr, secondArr) {
-    for (let i =0; i < secondArr.length; i += 1) {
-        console.log (firstArr[i], secondArr[i]);
+    for (let i = 0; i < secondArr.length; i += 1) {
+        for (let a = 0; a < firstArr.length; a += 1){
+            if (firstArr[a] === secondArr [i]) {
+                firstArr.splice(firstArr[a], 1);
+            }
+        }
     }
+    console.log('Массив студентов, который сдали экзамен:');
+    console.log(firstArr);
 }
-
 filter (allStudents, failedStudents);
-
-
-
-
-
-
-
 }
--- Task02.js
+++ Task02.js
@@ -1,12 +1,12 @@
 'use strict';
 {
-    const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
-    const mediumCash = arr => {
-        let result = 0;
-        for (const i of arr) {
-            result += i;
-        }
-        return console.log (`Средний чек за день: ${Math.floor(result / arr.length)} рублей`);
+const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
+const mediumCash = arr => {
+    let result = 0;
+    for (const i of arr) {
+        result += i;
     }
-    mediumCash(allСashbox);
+    return console.log (`Средний чек за день: ${Math.floor(result / arr.length)} рублей`);
+}
+mediumCash(allСashbox);
 }
