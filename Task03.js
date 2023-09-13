a2ea4c8c94c1277acfb6fa4b2d796b8a054f3e4d
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-13 10:18:04 +0300

    третья с правкой

--- Task03.js
+++ Task03.js
@@ -3,9 +3,9 @@
 const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
 function addPrefix(arr, prefSymbol) {
     for (let a = 0; a < arr.length; a += 1) {
-        arr[a] = (prefSymbol + " " + arr[a]);
-        console.log(arr[a]);
+        arr[a] = (prefSymbol + ' ' + arr[a]);
     }
+    console.log(arr);
 }
 addPrefix(names, 'Mr');
 }
